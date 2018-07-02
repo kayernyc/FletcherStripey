61893a6677008b1fd901b9b82f6b17564f6aaa34
Kathrin <gypsy_kat@yahoo.com>
2018-07-02 19:44:12 -0400

    Initial Commit

---
+++ FletcherStripey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FletcherStripey
+//
+//  Created by KATHRIN AYER on 7/2/18.
+//  Copyright © 2018 happierplaces. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
